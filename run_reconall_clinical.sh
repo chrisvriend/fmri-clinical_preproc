d8979b3048a7ed73ecc93f2df4d8de3727846798
Chris Vriend <59793342+chrisvriend@users.noreply.github.com>
2024-03-08 10:49:49 +0100

    Update run_reconall_clinical.sh
    added if statement

--- run_reconall_clinical.sh
+++ run_reconall_clinical.sh
@@ -10,7 +10,6 @@
 #SBATCH -o FSclin_%A.log
 #SBATCH --mail-type=END,FAIL
 
-
 # usage instructions
 Usage() {
     cat <<EOF
@@ -64,30 +63,31 @@ freesurferdir=${derivativesdir}/freesurfer
 mkdir -p ${freesurferdir}
 export SUBJECTS_DIR=${freesurferdir}
 
-if [ ! -f ${bidsdir}/${subj}${sessionpath}anat/${subj}${sessionfile}${acquisition}_T1w.nii.gz ]; then 
-echo "ERROR! input anatomical scan does not exist" 
-echo ${subj}${sessionfile}${acquisition}_T1w.nii.gz
-exit
+if [ ! -f ${bidsdir}/${subj}${sessionpath}anat/${subj}${sessionfile}${acquisition}_T1w.nii.gz ]; then
+    echo "ERROR! input anatomical scan does not exist"
+    echo ${subj}${sessionfile}${acquisition}_T1w.nii.gz
+    exit
 fi
 
-recon-all-clinical.sh \
-    ${bidsdir}/${subj}${sessionpath}anat/${subj}${sessionfile}${acquisition}_T1w.nii.gz \
-    ${subj} \
-    ${ncores} \
-    ${freesurferdir}
+if [[ ! -f ${freesurferdir}/${subj}/mri/aseg.auto.mgz || ! -f ${freesurferdir}/${subj}/mri/wm.awegedit.mgz ]]; then
+    recon-all-clinical.sh \
+        ${bidsdir}/${subj}${sessionpath}anat/${subj}${sessionfile}${acquisition}_T1w.nii.gz \
+        ${subj} \
+        ${ncores} \
+        ${freesurferdir}
+else
+    echo "FreeSurfer already ran for ${subj}"
 
+fi
 
-
-# check if necessary FreeSufer files are there
-if [[ ! -f ${freesurferdir}/${subj}/mri/wmparc.mgz \
-|| ! -f ${freesurferdir}/${subj}/mri/aseg.mgz ]]; then
- echo
+# check if necessary FreeSurfer files are there
+if [[ ! -f ${freesurferdir}/${subj}/mri/wmparc.mgz ||
+    ! -f ${freesurferdir}/${subj}/mri/aseg.mgz ]]; then
+    echo
     echo "ERROR! FreeSurfer did not finish correctly"
     echo "abort script"
     exit
 fi
-
-
 
 for scan in orig rawavg orig_nu T1 nu; do
 
